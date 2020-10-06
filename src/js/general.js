caaa7f3275b023b790e2fdc527fc491d21015ec7
prikhodkin <info@prikhodkin.com>
2020-10-07 02:31:09 +0500

    Добавляет модалки

--- src/js/general.js
+++ src/js/general.js
@@ -1,6 +1,7 @@
 import $ from "jquery";
 import mask from "jquery-mask-plugin";
-
+import {Popup, PopupThanks } from "%modules%/modal/modal";
+const popups = document.querySelectorAll('.popup');
 
 import { Application } from "stimulus"
 import { definitionsFromContext } from "stimulus/webpack-helpers"
@@ -14,4 +15,7 @@ const initMask = () => {
   $(`[data-tel]`).mask("0 (000) 000-00-00", {placeholder: "+7 (___) ___-__-__"});
 }
 
+popups.forEach(function (popup) {
+  new Popup(popup);
+});
 initMask();
